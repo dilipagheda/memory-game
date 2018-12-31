04c466baa7e245506f140d1007875f01bd3eac21
Dilip <dilip_agheda@yahoo.com>
2018-12-31 12:36:22 +1100

    beautification of code with consistent intendation and removal of blank lines

--- js/app.js
+++ js/app.js
@@ -4,24 +4,26 @@
 let cardPair = []; //cardPair will hold a pair of cards that user is handling for a given turn
 let totalMoves = 0; //total number of moves in the current game
 let totalStars = 5; //initially, user has 5 stars
-let totalSeconds=0; // to keep track of seconds
-let totalMinutes=0; //to keep track of minutes
-let totalHours =0; //to keep track of hours
-let interval = setInterval(displayTimer,1000); //set timer here and get the reference in interval variable to clear the timer later on.
+let totalSeconds = 0; // to keep track of seconds
+let totalMinutes = 0; //to keep track of minutes
+let totalHours = 0; //to keep track of hours
+let interval = setInterval(displayTimer, 1000); //set timer here and get the reference in interval variable to clear the timer later on.
 //Call shuffleAndDisplay() on the first page load.
 shuffleAndDisplay();
 
 //A function to restart game that resets game data and re-shuffles card
-function restartGame(){
+function restartGame() {
     //clear the cardPair array and get it ready for the next move
-    while (cardPair.length) { cardPair.pop(); }
+    while (cardPair.length) {
+        cardPair.pop();
+    }
 
     //reset global variables
     totalMoves = -1;
     totalStars = 5;
-    totalSeconds=0;
-    totalMinutes=0;
-    totalHours =0;
+    totalSeconds = 0;
+    totalMinutes = 0;
+    totalHours = 0;
 
     //display moves and stars
     incrementMove();
@@ -34,7 +36,7 @@ function restartGame(){
 
     //start the timer
     clearInterval(interval);
-    interval = setInterval(displayTimer,1000);
+    interval = setInterval(displayTimer, 1000);
 }
 
 /*
@@ -43,13 +45,13 @@ function restartGame(){
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-function shuffleAndDisplay(){
+function shuffleAndDisplay() {
     //get an array of cards using jquery selector
     let cards = $('.deck .card');
     //reset classes from previous game
     cards.removeClass();
     cards.addClass('card');
-    
+
     //pass that array to shuffle method to shuffle it
     cards = shuffle(cards);
 
@@ -58,7 +60,7 @@ function shuffleAndDisplay(){
 }
 
 //A function that takes an array of card elements and render it on screen
-function renderHTML(cards){
+function renderHTML(cards) {
     //remove existing li elements from deck
     $('.deck').empty();
 
@@ -71,7 +73,8 @@ function renderHTML(cards){
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -96,28 +99,28 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
- //A click listener for a card
-$('.deck').on('click',function(event){
+//A click listener for a card
+$('.deck').on('click', function (event) {
     let target = event.target;
-    if(target.tagName !== 'LI'){
+    if (target.tagName !== 'LI') {
         return;
     }
     //if animation is on then do not process the click for better user experience
-    if($('.deck .card.animated').length > 0){
+    if ($('.deck .card.animated').length > 0) {
         return;
     }
     console.log("clicked");
 
     let cardClicked = $(target);
-    if(cardClicked.hasClass('match')){
+    if (cardClicked.hasClass('match')) {
         return;
     }
-    if(processCard(cardClicked)){
+    if (processCard(cardClicked)) {
         checkForMatch(cardClicked);
     }
 
     //check if player has won and if so display a modal with Congratulations message
-    if(checkIfWon()){
+    if (checkIfWon()) {
         //clear timer
         clearInterval(interval);
         //display modal
@@ -127,15 +130,16 @@ $('.deck').on('click',function(event){
 
 //A function to keep track of time and also to display timer on screen
 displayTimer();
-function displayTimer(){
+
+function displayTimer() {
     totalSeconds++;
-    if(totalSeconds>=60){
+    if (totalSeconds >= 60) {
         totalMinutes++;
-        totalSeconds=0;
+        totalSeconds = 0;
     }
-    if(totalMinutes>=60){
+    if (totalMinutes >= 60) {
         totalHours++;
-        totalMinutes=0;
+        totalMinutes = 0;
     }
 
     //Update screen elements with values
@@ -148,7 +152,7 @@ function displayTimer(){
 A function to increment move counter and display it on the page
 A move is considered when user reveals a pair (2 cards) and not when user has revealed just the first card of a pair.
 */
-function incrementMove(){
+function incrementMove() {
     totalMoves++;
     $('span.moves').text(`${totalMoves} Moves`);
     updateStars();
@@ -167,23 +171,23 @@ function incrementMove(){
      1 star: >40 moves
  
 */
-function updateStars(){
-    if(totalStars === 5 && totalMoves > 16){
+function updateStars() {
+    if (totalStars === 5 && totalMoves > 16) {
         totalStars--;
     }
-    if(totalStars === 4 && totalMoves > 24){
+    if (totalStars === 4 && totalMoves > 24) {
         totalStars--;
     }
-    if(totalStars === 3 && totalMoves > 32){
+    if (totalStars === 3 && totalMoves > 32) {
         totalStars--;
     }
-    if(totalStars === 2 && totalMoves > 40){
+    if (totalStars === 2 && totalMoves > 40) {
         totalStars--;
     }
 
     //render on screen
     $('ul.stars').empty();
-    for(let i=0;i<totalStars;i++){
+    for (let i = 0; i < totalStars; i++) {
         let starElement = $('<li><i class="fa fa-star"></i></li>');
         $('ul.stars').append(starElement);
     }
@@ -191,27 +195,27 @@ function updateStars(){
 }
 
 //A function to process the card that was clicked
-function processCard(card){
+function processCard(card) {
     console.log(card.prop('class'));
 
     //if card has any of the following classes then do not process the selection
-    if(card.hasClass('open') ||  card.hasClass('show') || card.hasClass('no-match') || card.hasClass('animated')){
-        return false; 
-    }else{
+    if (card.hasClass('open') || card.hasClass('show') || card.hasClass('no-match') || card.hasClass('animated')) {
+        return false;
+    } else {
         showCard(card);
         return true;
     }
 }
 
 //A function to show card with animation
-function showCard(card){
+function showCard(card) {
     setNotAllowedOnCards();
     card.addClass('open show animated swing');
     //remove animated classes after 1 sec
-    setTimeout(function(){
+    setTimeout(function () {
         removeNotAllowedOnCards();
-        card.removeClass('animated swing');  
-    },1000);
+        card.removeClass('animated swing');
+    }, 1000);
 }
 
 /*
@@ -220,35 +224,35 @@ function showCard(card){
  This is to avoid too many clicks and so prevent the screen in resulting too many animations at the same time.
  Assuming that app consumers will be children, its good to add such logic to prevent too many random clicks
 */
-function setNotAllowedOnCards(){
-  $('.deck .card').addClass('not-allowed');
+function setNotAllowedOnCards() {
+    $('.deck .card').addClass('not-allowed');
 }
 
 /*
  A function to remove cursor - not allowed on all card elements. 
 */
-function removeNotAllowedOnCards(){
+function removeNotAllowedOnCards() {
     $('.deck .card').removeClass('not-allowed');
 }
 
 //A function to hide cards 
-function hideCards(card1,card2){
+function hideCards(card1, card2) {
     card1.removeClass();
     card2.removeClass();
     setNotAllowedOnCards();
     card1.addClass('card animated bounce');
     card2.addClass('card animated bounce');
-    setTimeout(function(){
+    setTimeout(function () {
         card1.removeClass();
         card2.removeClass();
         card1.addClass('card');
-        card2.addClass('card');    
+        card2.addClass('card');
         removeNotAllowedOnCards();
-    },1000);
+    }, 1000);
 }
 
 //A function to mark a card as matched
-function matchCards(card1,card2){
+function matchCards(card1, card2) {
     card1.removeClass('open show animated swing');
     card2.removeClass('open show animated swing');
     setNotAllowedOnCards();
@@ -257,16 +261,16 @@ function matchCards(card1,card2){
     card2.addClass('match animated rubberBand');
 
     //remove animation classes after 1 sec
-    setTimeout(function(){
+    setTimeout(function () {
         card2.removeClass('animated rubberBand');
         card1.removeClass('animated rubberBand');
         removeNotAllowedOnCards();
-    },1000);
+    }, 1000);
 
 }
 
 //A function to change background color to red and do no match animation
-function unmatchCards(card1,card2){
+function unmatchCards(card1, card2) {
     card1.removeClass('open show animated swing');
     card2.removeClass('open show animated swing');
 
@@ -275,48 +279,49 @@ function unmatchCards(card1,card2){
     card1.addClass('no-match show animated shake');
     card2.addClass('no-match show animated shake');
 
-    setTimeout(function(){
-        hideCards(card1,card2);
+    setTimeout(function () {
+        hideCards(card1, card2);
         removeNotAllowedOnCards();
-    },1000);
+    }, 1000);
 }
 
 //A function to check if a card was matched or not
-function checkForMatch(card){
-    if(cardPair.length === 0){
+function checkForMatch(card) {
+    if (cardPair.length === 0) {
         cardPair.push(card);
-    }else if(cardPair.length > 0){
-         let totalClassesMatched=0;
-         let firstCard = cardPair[0];
-         let secondCard = card;
-         let firstCardClassListValues = firstCard.find('i').prop("classList").values();
-       
-         for(let value1 of firstCardClassListValues){
-                let secondCardClassListValues = secondCard.find('i').prop("classList").values();
-                for(let value2 of secondCardClassListValues)
-                {
-                    if(value1 === value2){
-                     totalClassesMatched++;
-                    }
-                 }
-         }
+    } else if (cardPair.length > 0) {
+        let totalClassesMatched = 0;
+        let firstCard = cardPair[0];
+        let secondCard = card;
+        let firstCardClassListValues = firstCard.find('i').prop("classList").values();
 
-         if(totalClassesMatched===2){
-             matchCards(firstCard,secondCard);
-         }else{
-            unmatchCards(firstCard,secondCard);
+        for (let value1 of firstCardClassListValues) {
+            let secondCardClassListValues = secondCard.find('i').prop("classList").values();
+            for (let value2 of secondCardClassListValues) {
+                if (value1 === value2) {
+                    totalClassesMatched++;
+                }
+            }
+        }
 
-         }
-         //a move (or turn) is done here so increment the move counter
-         incrementMove();
-         //clear the cardPair array and get it ready for the next move
-         while (cardPair.length) { cardPair.pop(); }
+        if (totalClassesMatched === 2) {
+            matchCards(firstCard, secondCard);
+        } else {
+            unmatchCards(firstCard, secondCard);
+
+        }
+        //a move (or turn) is done here so increment the move counter
+        incrementMove();
+        //clear the cardPair array and get it ready for the next move
+        while (cardPair.length) {
+            cardPair.pop();
+        }
     }
 }
 
 
 //A function to check if the player has won or not
-function checkIfWon(){
+function checkIfWon() {
     //get the total of card elements 
     let total = $('.deck .card').length;
 
@@ -324,31 +329,31 @@ function checkIfWon(){
     let totalMatched = $('.deck .card.match').length;
 
     //check if they are equal and if so then game is won
-    if(total===totalMatched){
+    if (total === totalMatched) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 //A function to display congratulate message
-function displayCongratulateMessage(){
+function displayCongratulateMessage() {
     //prepare the message variables with values
     let message1 = `With ${totalMoves} Moves and ${totalStars} Stars!`;
-    let message2 = `Total time taken was ${totalHours} hours, ${totalMinutes} minutes and ${totalSeconds} seconds.`;              
+    let message2 = `Total time taken was ${totalHours} hours, ${totalMinutes} minutes and ${totalSeconds} seconds.`;
     let message3 = `Woooooo!`;
     //display values to screen elements
     $('.result-msg-1').text(message1);
     $('.result-msg-2').text(message2);
     $('.result-msg-3').text(message3);
 
-    $('.modal').css('display','block');
+    $('.modal').css('display', 'block');
 }
 
 //A click listener for play again button on modal screen and refresh button on screen
-$('.restart').on('click',function(){
+$('.restart').on('click', function () {
     console.log('restart');
     restartGame();
     //close the modal
-    $('.modal').css('display','none');
+    $('.modal').css('display', 'none');
 });
